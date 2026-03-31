1e714712c9674436b7f560a95cad8728dcfaef2b
estebanghinamo <163152792+estebanghinamo@users.noreply.github.com>
2026-03-31 18:22:18 -0300

    cdas

---
+++ GriffonVet/src/main/java/griffonvet/griffonvet/GriffonVetApplication.java
@@ -0,0 +1,13 @@
+package griffonvet.griffonvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriffonVetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GriffonVetApplication.class, args);
+    }
+
+}
